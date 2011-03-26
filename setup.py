cd768e127646bd55bff0824cca018680f7caf8df
nickm <nickm>
2011-03-26 16:12:28 +0000

    scripts 0.4.43

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='nec',
-      version='0.4.40',
+      version='0.4.43',
       author='Nikolay Mladenov',
       author_email='Nikolay dot Mladenov at gmail dot com',
       packages=['nec'],
