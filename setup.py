2abd0e59cce7fc15ccffea2544f1b8360c20b4b5
nickm <nickm>
2014-09-06 03:50:19 +0000

    attempt to sync scripts versioning

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 setup(name='nec',
-      version='0.14.118',
+      version='0.14.119',
       author='Nikolay Mladenov',
       author_email='Nikolay dot Mladenov at gmail dot com',
       packages=['nec'],
